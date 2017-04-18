dc53ad0f9a06feef93bf6e30a9903a564360bdbc
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-04-18 12:31:16 +0200

    Add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss')
     mix.sass('admin.scss')
-    
+
     mix.copy('resources/assets/images', 'public/images')
-        .version('public/images');
 
     mix.copy('resources/assets/css', 'public/css')
-        .version('public/css');
+
+    mix.copy('resources/assets/favicon', 'public/favicon')
 });
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,14 +2,31 @@
 <html lang="en">
 
 <head>
-    <title>À prendre en main | Administration</title>
+    <title>U&D | Administration</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-    <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon.png">
     <link rel="stylesheet" href="/css/admin.css">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
 </head>
 
 <body class="admin-page">
@@ -48,7 +65,7 @@
     <!-- END MAIN CONTENT -->
         <footer>
             <div class="container-fluid">
-                <p class="copyright">&copy; 2017. All credits for A Prendre En Main</p>
+                <p class="copyright">&copy; 2017. All credits for U&D</p>
             </div>
         </footer>
     </div>
