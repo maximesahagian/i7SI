bdba4715cccaf5450322c78df612920e532dbaeb
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-04-19 14:57:49 +0200

    Add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <ul class="list">
+        <li>À propos</li>
+        <li>Conditions</li>
+        <li>Politique de confidentialité</li>
+        <li>Contact</li>
+    </ul>
+</footer>
