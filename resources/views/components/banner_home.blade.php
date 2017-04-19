7637a3373ba4ebb68194f9dc865290d5375b284b
kevin nabeth <kgnabeth@gmail.com>
2017-04-19 16:40:29 +0200

    Header

--- resources/views/components/banner_home.blade.php
+++ resources/views/components/banner_home.blade.php
@@ -1,8 +1,9 @@
 <section class="banner-home">
     <div class="banner-container">
         <div class="banner-content">
-            <h2>Lorem ipsum dolor sit amet,<br>
-                consecteur adipiscing
+            <h2>Accompagner nos client <br>
+                et leur offrir un travail de qualité
+
             </h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
             <div class="discover">
