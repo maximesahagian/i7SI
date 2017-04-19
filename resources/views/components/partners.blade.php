d6a7f5b6fc12faf6c6900be54ef72ed14eb293c1
robin <robin.belvedere@hetic.net>
2017-04-19 18:52:41 +0200

    Hover bloc partners

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -5,21 +5,16 @@
                 <h2 class="title-partners">Nos partenaires</h2>
             </div>
         </div>
-        <div class="bloc-container">
-            <div class="partenaires partenaire-1"></div>
+        <div class="bloc-container partenaires partenaire-1">
         </div>
-        <div class="bloc-container">
-            <div class="partenaires partenaire-2"></div>
+        <div class="bloc-container partenaires partenaire-2">
         </div>
-        <div class="bloc-container">
-            <div class="partenaires partenaire-3"></div>
+        <div class="bloc-container partenaires partenaire-3">
         </div>
-        <div class="bloc-container">
+        <div class="bloc-container partenaires partenaire-4">
 
-            <div class="partenaires partenaire-4"></div>
         </div>
-        <div class="bloc-container">
-            <div class="partenaires partenaire-5"></div>
+        <div class="bloc-container partenaires partenaire-5">
 
         </div>
     </div>
