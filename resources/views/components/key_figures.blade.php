322e6b2bfce810702a0ec35df3c3c58f9d874f37
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-04-19 12:35:58 +0200

    Add key figures integration

--- resources/views/components/key_figures.blade.php
+++ resources/views/components/key_figures.blade.php
@@ -2,28 +2,54 @@
     <h3 class="title-key">LES CHIFFRES CLÉS & ACTUALITÉS</h3>
     <div class="key-container">
         <div class="key-block black-block">
-
+            <div class="key-block-content">
+                <p class="breadcrumb">LOREMP INFORMATIONS <span class="gray-span">/ 18.04</span></p>
+                <h4 class="title">Loremp Ipsum title</h4>
+                <p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                    laudantium, totam rem aperiam, eaque ipsa qu</p>
+            </div>
         </div>
         <div class="key-block dark-gray-block">
-
+            <div class="key-block-content">
+                <p class="breadcrumb">LOREMP INFORMATIONS <span class="gray-span">/ 18.04</span></p>
+                <h4 class="title">Loremp Ipsum title</h4>
+                <p class="text"><span class="big-gray-span">2 700 000 €</span></p>
+            </div>
         </div>
         <div class="key-block white-block">
-
+            <div class="key-block-content">
+                <p class="breadcrumb">LOREMP INFORMATIONS <span class="gray-span">/ 18.04</span></p>
+                <h4 class="title black">Loremp Ipsum title</h4>
+                <p class="text black">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore totam rem aperiam, atus
+                    error sit voluptatem accusantium doloremque laudantium, totam</p>
+            </div>
         </div>
         <div class="key-block arrow-right-block">
 
         </div>
         <div class="key-block gray-gradiant-block">
+            <div class="key-block-content">
 
+            </div>
         </div>
         <div class="key-block white-block">
-
+            <div class="key-block-content">
+                <p class="breadcrumb">LOREMP INFORMATIONS <span class="gray-span">/ 18.04</span></p>
+                <h4 class="title black">Loremp Ipsum dajuk subtitle le lorem kurt</h4>
+            </div>
         </div>
         <div class="key-block classroom-block">
 
         </div>
         <div class="key-block orange-block">
-
+            <div class="key-block-content">
+                <p class="breadcrumb breadcrumb-white">LOREMP INFORMATIONS <span class="gray-span">/ 18.04</span></p>
+                <h4 class="title">Loremp Ipsum title</h4>
+                <p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore totam rem aperiam, atus
+                    error sit voluptatem accusantium doloremque laudantium, totam</p>
+            </div>
         </div>
     </div>
 </section>
