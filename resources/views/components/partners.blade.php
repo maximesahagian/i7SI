83512e218467acac53688cdd725048d3595253aa
golfw <belvedererobin@gmail.com>
2017-04-19 11:15:17 +0200

    bloc partenaires

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -1,3 +1,10 @@
 <section class="partners">
-
+    <div class="container">
+        <div class="partenaires partenaire-1"></div>
+        <div class="partenaires partenaire-2"></div>
+        <div class="partenaires partenaire-3"></div>
+        <div class="partenaires partenaire-4"></div>
+        <div class="partenaires partenaire-5"></div>
+        <div class="partenaires partenaire-6"></div>
+    </div>
 </section>
